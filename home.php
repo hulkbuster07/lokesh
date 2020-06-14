46ea1b3606a95782117a6150bce2ffefe80bdff1
hulkbuster07 <66904704+hulkbuster07@users.noreply.github.com>
2020-06-14 16:14:58 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php 
+	include 'boot.php'; 
+    include 'nav.php';
+	?>
+	<title></title>
+</head>
+<body>
+
+	
+		<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="intro-bg_1.jpg" class="d-block w-100" alt="..." >
+       <center><div class="carousel-caption"><h2 style="padding-bottom: 600px; color: black; font-size: 50px;" >welcome ,u can shop now</h2></div></center> 
+    </div>
+    
+   
+  </div>
+</div>
+	
+
+
+	
+
+
+
+<?php 
+include 'footer.php'; ?>
+</body>
+</html>
